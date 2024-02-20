b794b9d37276a1e56d5a6fd82a4a706fa06a1332
kiaaww <155945180+kiaaww@users.noreply.github.com>
2024-02-20 20:39:01 +0200

    Delete viikko7 directory

--- viikko7/database.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'hh',
-  database: 'test_db'
-});
-module.exports = connection;
